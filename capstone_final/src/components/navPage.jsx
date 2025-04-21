6c562c329efcf9730f3f5ac8203dd575803c1c50
sheltrob000 <sheltrob000@warren.k12.in.us>
2025-04-21 12:24:17 -0400

    it works cinda

--- capstone_final/src/components/navPage.jsx
+++ capstone_final/src/components/navPage.jsx
@@ -6,14 +6,17 @@ export default function NavPage() {
 
 
     return(
-        <div className={"hello"}>
-            <button className={"navpage"} onClick={setPage("home")}>home</button>
-            {/*<button className={"navpage"} onClick={handleSetPage("wiki")}>wiki</button>*/}
-            {/*<button className={"navpage"} onClick={handleSetPage("map")}>map</button>*/}
-            <button className={"navpage"} onClick={() => console.log("hello")}>hello</button>
+        <>
+            <div className={"hello"}>
+                <button className={"navpage"} onClick={() => setPage("home")}>home</button>
+                <button className={"navpage"} onClick={() => setPage("wiki")}>wiki</button>
+                <button className={"navpage"} onClick={() => setPage("map")}>map</button>
+                <button className={"navpage"} onClick={() => console.log("hello")}>hello</button>
+            </div>
 
             <p>{page}</p>
 
-        </div>
+        </>
+
     )
 }
